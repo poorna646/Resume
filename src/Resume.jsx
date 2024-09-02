effbd60826fca01e7943508bb4b5d70c6f1dcc12
POORNASHREE T <107635736+poorna646@users.noreply.github.com>
2024-09-02 17:48:45 +0530

    Add files via upload

---
+++ src/Resume.jsx
@@ -0,0 +1,98 @@
+import './style.css';
+import poorna from './poorna.jpg'
+export default function Resume()
+{
+    return(
+        <div class="border">
+            <div class="container">
+            <div className='side'>
+            
+            <img className="poorna"src={poorna} alt="" />            
+
+            <h3><b>CONTACTS</b></h3>
+            <p><i class="fa-solid fa-phone"></i>7094616477</p>
+            <p><i class="fa-solid fa-envelope"></i>poornashree646@gmail.com</p>
+            <p><i class="fa-sharp fa-solid fa-location-dot"></i>D/no-5/806-2A,4th   
+            cross,<br /> sivakumar nagar, Hosur-635109</p>
+            <a href="https://www.linkedin.com/in/poornashree-thangeswaran-40396824b/"><i class="fa-brands fa-linkedin"></i>https://www.linkedin.com/in/<br/>poornashree-thangeswaran-40396824b/</a>
+            <br />
+            <img src="data:image/png;charset=utf-8;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAACXBIWXMAAAsTAAALEwEAmpwYAAACgklEQVR4nGNgIBH4GEtyGWrJvFNTU/lvbyx5goGawMJChlNPQ/azurrafxDWVFP4TTPD1dRU/zubSi2miuEqKgzsepqyH2GGg7CtkdQ2ahr+CdlwL2uZvVQzXFcD1fCWNM3/L9db/3ux2vjw//3yHJQZrokaLI0pmv9fbbCB45drTV/dWmzGR475bLoaciiGp/orvXm5xmj/i9Um919tsEa25MXLVVo8JBou+wHZcDcL6UPICp6v0i97tR7JkjWmz/6v0mIjy3AvK5kj2BQ+X2WY+3K91T+4Jav1Gwiarq8p+xrZcD9bmeP41L9Yo5/2cp3Vv5frLf8+X21sjtdwRxOJJciGBzjInCboIgYGhrurjPn/L9LjJqhQQ1X+F8xwd0vZ2wxUBmyqqirQIkD5/8w0Bi5kyS0bNjhu3rjh/eaNG/4Tgf9s3rBhDorpHBwcsiIiwv9BWE9N5A+67cuWLN60ZNHC/8TjBb/QzWAU5uf4z87O9p+fl/1/ko+QG4oFyxaZL5o/787C+fMeE4MXL144GSOMLDS5P4AsAGFLTe5fnUnCUhQGOypwNeGLEOBjB1sAwra6PD/rQ7kkCOm7OUu28eZs+WqiLHEy5FvIzYViyY/KCG5xXOrvzpY7/HyJ/H8QvjNTNp0oSxz1eZYjW2Kjy/0jN5RHFF3dvblyJ2GGg/CN2bLhRFkA9okBz0pkS+z0eH6c6pbRAcndmCkpcm+ezFVkw2/Pkp3LQCpw1udejWzJ2Ymy/x
+            4vlPvxdLHcP2TDb82WXUSy4TDgasy7UlqU43+wDd//Z4sRhoLwM0oNh4Ebs6Sd7s6Tu/F4gdwfkCWPF8n9vT9P9uH12TJRxBgAABE1rBTkH+ppAAAAAElFTkSuQmCC" alt=''/>
+            <a href="https://leetcode.com/Poornashree_646/">https://leetcode.com/<br/>Poornashree_646/</a>
+            <br />
+            <a href="https://github.com/poorna646"><i class="fa-brands fa-github"></i>https://github.com/poorna646</a>
+            
+            
+            <h3><b>AREA OF INTEREST</b></h3>
+            <p>DBMS</p>
+            <p>Java</p>
+
+            <h3><b>TOOLS AND TECH KNOWN</b></h3>
+            <p>c</p>
+            <p>Java</p>
+            <p>Python</p>
+            <p>Figma</p>
+            <p>HTML,CSS,Js</p>
+            <p>Mongodb</p>
+            <p>React(basics)</p>
+            <p>SQL</p>
+
+            <h3><b>ACHIVEMENTS</b></h3>
+            <p>Presented a paper on chatbot using LLM with implementation and won First prize in Sri Ramakrishna engineering college.</p>
+
+            
+
+            <h3><b>LANGUAGES KNOWN</b></h3>
+            <p>English(SRW)</p>
+            <p>Tamil(SRW)</p>
+            <p>Kannada(S)</p>
+            </div>
+
+            <div className='side1'>
+            <h2><b>POORNASHREE T</b></h2>          
+            <h4>B.E COMPUTER SCIENCE AND ENGINNERING COLLEGE</h4>
+            <h3><b>OBJECTIVE</b></h3>
+            <p>I'm eager to work and develop various skills.<br/> I love to be passionate towards work.</p>
+
+            <h3><b>PROJECTS</b></h3>
+            <h5>Developed a chatbot using RAG for titan company</h5>
+            <p>Developed with the knowledge of machine learning -LLM,RAG and in-buit libraries.</p>
+            <a href="https://github.com/poorna646/Mistral_rag"><i>https://github.com/poorna646/Mistral_rag</i></a>
+
+            <h5>Developed a Number guessing game using java</h5>
+            <p>Developed with the knowledge of machine learning -LLM,RAG and in-buit libraries.</p>
+            <a href="https://github.com/poorna646/Java/blob/main/NumerGame"><i>https://github.com/poorna646/Java/blob/main/NumerGame</i></a>
+
+            <h3><b>EDUCATIONAL BACKGROUND</b></h3>
+            <p><h6>KONGU ENGINEERING COLLEGE, PERUNDURAI</h6></p>
+            <p>Currently pursuing BE Computer Science with aggregate of 8.31CGPA</p>
+
+            <p><h6>MAHARISHI VIDYA MANDIR SENIOR SECONDARY SCHOOL , HOSUR</h6></p>
+            <p>completed HSC with 94.6% in 2021</p>
+
+            <p><h6>MAHARISHI VIDYA MANDIR SENIOR SECONDARY SCHOOL , HOSUR</h6></p>
+            <p>completed SSLC with 89.4% in 2019</p>
+
+            
+
+            <h3>PAPER AND PROJECT PRESENTED</h3>
+            <h4>Presented on NeuraLink at CIT(24-03-2023).</h4>
+            <p>presented about a brain-computer interface called “the Link,” which is a surgically embedded neural-chip implant designed to decode and stimulate brain activity.
+            </p>
+
+            <h4> Participated in ideathon conducted in our college</h4>
+            <p>presented an idea about a cleanliness detection device <br /> to identify unclean restrooms, notifying authorities through a website.<br /> Implemented with Arduino microcontroller and various sensors for efficient detection.</p>
+
+            <h4>Participated in SIH 2023</h4>
+            <p>presented an idea on the problem statement application of Image Analytics<br /> for Tree enumeration for diversion of Forest Land.</p>
+
+            <h3>SOFT SKILL</h3>
+            <p>Team work, Leadership quality, Design Thinking </p>
+            </div>
+            </div>
+
+
+        
+        </div>
+    );
+}
